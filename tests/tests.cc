1efca6f23e7eb3a7522d560569adbae90b666b60
jianglingye <andyj2@illinois.edu>
2026-02-27 22:44:39 +0000

    third try

--- tests/tests.cc
+++ tests/tests.cc
@@ -94,5 +94,5 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-4]") {
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
   REQUIRE_THROWS_AS(atm.PrintLedger("./prompt.txt", 00000000, 0000),
                     std::invalid_argument);
-  REQUIRE_THROWS(atm.PrintLedger(".   ", 12345678, 1234));
+  REQUIRE_THROWS(atm.PrintLedger(".  ", 12345678, 1234));
 }
